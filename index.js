b28c9a46078272348e609f96759536e984ff1dcc
Meili Vanegas Hernández <m.vanegas10@uniandes.edu.co>
2017-05-11 19:20:24 -0500

    Add on postgres try 3

--- index.js
+++ index.js
@@ -82,26 +82,26 @@ io.on('connection', function(socket) {
         getROP(socket.id);        
         // getData(socket.id, 'red_primaria', 'polyline', '"gid" IN (176,784,794,793,798,796,822,819,856,852,849,885,894,891,937,932,938,984,990,986,1029,1028,1076,1077,1113,1114,1117,1165,1164,1218,1221,1220,1280,1281,1284,1332,1330,1373,1368,1374,1418,1416,1455,1453,1487,1533,1527,51,48,52,64,63,76,94,91,90,96,102,101,106,110,109,114,113,118,117,122)');
         getData(socket.id, 'red_primaria', 'polyline');
-        getAdditionalData(socket.id, 'bienestar', 'point');
-        getAdditionalData(socket.id, 'culto', 'point');
-        getAdditionalData(socket.id, 'cultura', 'point');
-        getAdditionalData(socket.id, 'cysf', 'point');
-        getAdditionalData(socket.id, 'deportes', 'point');
-        getAdditionalData(socket.id, 'educacion', 'point');
-        getAdditionalData(socket.id, 'edusup', 'point');
-        getAdditionalData(socket.id, 'recintos_feriales', 'point');
-        getAdditionalData(socket.id, 'sa', 'point');
-        getAdditionalData(socket.id, 'salud', 'point');
-        getAdditionalData(socket.id, 'seguridad', 'point');
-        getAdditionalData(socket.id, 'land', 'point');
-        getInitialState(socket.id);
+        getData(socket.id, 'bienestar', 'point');
+        getData(socket.id, 'culto', 'point');
+        getData(socket.id, 'cultura', 'point');
+        getData(socket.id, 'cysf', 'point');
+        getData(socket.id, 'deportes', 'point');
+        getData(socket.id, 'educacion', 'point');
+        getData(socket.id, 'edusup', 'point');
+        getData(socket.id, 'recintos_feriales', 'point');
+        getData(socket.id, 'sa', 'point');
+        getData(socket.id, 'salud', 'point');
+        getData(socket.id, 'seguridad', 'point');
+        getData(socket.id, 'land', 'point');
+        getState(socket.id, 0);
         console.log(':: This is a ' + glbs.INITIALIZE + ' request...');
     });
 
     socket.on(glbs.PREVIOUS, function(current) {
         console.log(':: This is a ' + glbs.PREVIOUS + ' request...');
         getSegregationIndex(socket.id,current - 1);
-        getPrevious(socket.id, current);
+        getState(socket.id, current-1);
     });
 
     socket.on(glbs.NEXT, function(current) {
@@ -110,7 +110,7 @@ io.on('connection', function(socket) {
         //     getData(socket.id, 'red_primaria', 'polyline', '"gid" IN (985,974,973,1022,1020,1018,1015,1014,1013,1011,1002,1001,1006,1005,996,994,442,443,443,435,434,427,425,420,419,417,416,415,412,411,487,483,472,471,468,463,530,578)');
         // }
         getSegregationIndex(socket.id,current + 1);
-        getNext(socket.id, current);
+        getState(socket.id, current+1);
     });
 });
 
@@ -118,13 +118,12 @@ io.on('connection', function(socket) {
 // Functions
 // ------------------------------------------------------
 
-
-function getAdditionalData(socketId, table, type) {
-    var query = 'SELECT *, ST_AsText(geom) AS wkt FROM ' + table;
-    // + ' WHERE "codigo_upz" IN (85,81,80,46,112,116,31,30,29,28,27)';
-    var parameters = {
-        querystring: query,
-    };
+function getData(socketId, table, type, specifics) {
+    var query;
+    if(specifics) 
+        query = 'SELECT *, ST_AsText(geom) AS wkt FROM ' + table + ' WHERE ' + specifics;
+    else
+        query = 'SELECT *, ST_AsText(geom) AS wkt FROM ' + table;
 
     makeQuery(query).then(function (result){
         var msg = {};
@@ -143,105 +142,53 @@ function getAdditionalData(socketId, table, type) {
               catch (err){}
         }
         msg.table = table;
-        console.log(msg);
-        // clients[socketId].emit(glbs.SHOW_ADD_DATA, msg);
+        clients[socketId].emit(glbs.SHOW_DATA, msg);
     })
-    .catch(function (err){ console.log(err);});
-    // pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    //     client.query(query, function(err, result) {
-    //         done();
-    //         if (err){ 
-    //             console.error(err); response.send("Error " + err); 
-    //         }
-    //         else{ 
-    //             var msg = {};
-    //             msg.type = 'point';
-    //             msg.features = [];
-    //             for (var i = 0; i < result.length; i++) {
-    //                 try{
-    //                     var geometry = parse(result[i].wkt);
-    //                     var properties = {};
-    //                     var type = "Feature";
-    //                     properties.gid = result[i].gid;
-    //                     properties.type = table;
-    //                     var feature = {geometry:geometry,properties:properties,type:type};
-    //                     msg.features.push(feature);
-    //                   }
-    //                   catch (err){}
-    //             }
-    //             msg.table = table;
-    //             clients[socketId].emit(glbs.SHOW_ADD_DATA, msg); // Sending to the client the new event...
-    //         }
-    //     });
-    // });    
+    .catch(function (err){ console.log(err);}); 
 }
 
-function getData(socketId, table, type, specifics) {
-    var parameters = {};
-    if (specifics !== undefined) {
-        parameters = {
-            geometry: 'geom',
-            tableName: table,
-            properties: 'all',            
-            where: specifics,
-            debug: '',
-        };
-    }
-    else {
-        parameters = {
-            geometry: 'geom',
-            tableName: table,
-            properties: 'all',
-            debug: '',
-        };
-    }
-
-    geo.geoQuery(parameters, function(json) {
-        json.type = type;
-        json.table = table;
-        clients[socketId].emit(glbs.SHOW_DATA, json); // Sending to the client the new event...
-    });
-
-}
-
-function getInitialState(socketId) {
-    var parameters = {
-        geometry: 'geom',
-        tableName: 'properties_state',
-        where: '"step" = \'0\'',
-        properties: 'all',
-    };
-
-    geo.geoQuery(parameters, function(json) {
-        json.Time = 0;
-        fs.appendFile('/Users/Meili/Desktop/dataBuena.json', JSON.stringify(json), 'utf8', function(err) {
-            if (err) {
-                return console.log(err);
-            }
-        });
-        clients[socketId].emit(glbs.DRAW_MAP, json); // Sending to the client the new event...
-    });
+function getState(socketId, step) {
+    var query = 'SELECT *, ST_AsText(geom) AS wkt FROM ' + table + ' WHERE step = \'' + step + '\'';
+    makeQuery(query).then(function (result){
+        var msg = {};
+        msg.type = 'point';
+        msg.features = [];
+        for (var i = 0; i < result.length; i++) {
+            try{
+                var geometry = parse(result[i].wkt);
+                var properties = {};
+                var type = "Feature";
+                properties.gid = result[i].gid;
+                properties.type = table;
+                var feature = {geometry:geometry,properties:properties,type:type};
+                msg.features.push(feature);
+              }
+              catch (err){}
+        }
+        msg.table = table;
+        msg.Time = 0;
+        // fs.appendFile('/Users/Meili/Desktop/dataBuena.json', JSON.stringify(json), 'utf8', function(err) {
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+        // });        
+        clients[socketId].emit(glbs.DRAW_MAP, msg);
+    })
+    .catch(function (err){ console.log(err);});     
 }
 
 function getROP(socketId) {
     var query = "SELECT * FROM indicator1";
-    var parameters = {
-        querystring: query,
-        debug: ''
-    };
-    geo.query(parameters, function(json) {
-        clients[socketId].emit(glbs.I1_CHART, json); // Sending to the client the new event...
-    });
+    makeQuery(query).then(function (result){
+        clients[socketId].emit(glbs.I1_CHART, result); // Sending to the client the new event...
+    })
+    .catch(function (err){ console.log(err);});      
 }
 
 function getSegregationIndex(socketId, time) {
     var query = 'SELECT i.si,i.step,u.*, ST_AsText(u.geom) AS wkt FROM indicator2 i INNER JOIN upz u ON i."idUPZ" = u.gid WHERE step = ' + time;
     console.log(query);
-    var parameters = {
-        querystring: query,
-        debug: ''
-    };
-    geo.query(parameters, function(json) {
+    makeQuery(query).then(function (json){
         var msg = {};
         msg.type = 'multipolygon';
         msg.features = [];
@@ -268,47 +215,6 @@ function getSegregationIndex(socketId, time) {
         }
         msg.table = 'upz';
         clients[socketId].emit(glbs.SHOW_DATA, msg); // Sending to the client the new event...
-    });
-}
-
-function getPrevious(socketId, current) {
-    var parameters = {
-        geometry: 'geom',
-        tableName: 'properties_state',
-        where: '"step" = \'' + (current - 1) + '\'',
-        properties: 'all',
-    };
-
-    geo.geoQuery(parameters, function(json) {
-        json.Time = current - 1;
-        clients[socketId].emit(glbs.DRAW_MAP, json); // Sending to the client the new event...
-    });
-}
-
-function getNext(socketId, current) {
-    var parameters = {
-        geometry: 'geom',
-        tableName: 'properties_state',
-        where: '"step" = \'' + (current + 1) + '\'',
-        properties: 'all',
-    };
-
-    geo.geoQuery(parameters, function(json) {
-        json.Time = current + 1;
-        clients[socketId].emit(glbs.DRAW_MAP, json); // Sending to the client the new event...
-    });
-}
-
-function getSpecific(socketId, specificTime) {
-    var parameters = {
-        geometry: 'geom',
-        tableName: 'properties_state',
-        where: '"step" = \'' + specificTime + '\'',
-        properties: 'all',
-    };
-
-    geo.geoQuery(parameters, function(json) {
-        json.Time = specificTime;
-        clients[socketId].emit(glbs.DRAW_MAP, json); // Sending to the client the new event...
-    });
+    })
+    .catch(function (err){ console.log(err);}); 
 }
